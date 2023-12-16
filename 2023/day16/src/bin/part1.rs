c011a043e7c79670e3688d2a6e741fbb035b4f84
Vasco Schwarze <leandervasco.schwarze@sciota.io>
2023-12-16 23:58:23 +0100

    Added solutions for 2023/16

---
+++ 2023/day16/src/bin/part1.rs
@@ -0,0 +1,182 @@
+use std::{collections::HashSet, vec};
+
+#[derive(PartialEq, Clone, Eq, Hash, Copy, Debug)]
+enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+}
+
+#[derive(PartialEq, Debug)]
+enum Tile {
+    Empty,               // '.'
+    HorizontalSplitter,  // '-'
+    VerticalSplitter,    // '|'
+    RightDiagonalMirror, // '/'
+    LeftDiagonalMirror,  // '\'
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    // let input = include_str!("../../input_test.txt");
+    println!("{input}\n");
+
+    let grid: Vec<Vec<Tile>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    '.' => Tile::Empty,
+                    '-' => Tile::HorizontalSplitter,
+                    '|' => Tile::VerticalSplitter,
+                    '/' => Tile::RightDiagonalMirror,
+                    '\\' => Tile::LeftDiagonalMirror,
+                    _ => unreachable!(),
+                })
+                .collect()
+        })
+        .collect();
+
+    let total_rows = grid.len();
+    let total_cols = grid[0].len();
+
+    let mut beam_ends: Vec<(u32, u32, Direction)> = vec![(0, 0, Direction::Right)];
+    let mut energized_tiles: HashSet<(u32, u32, Direction)> = HashSet::new();
+    energized_tiles.insert((0, 0, Direction::Right));
+
+    while let Some(beam_end) = beam_ends.pop() {
+        match grid[beam_end.0 as usize][beam_end.1 as usize] {
+            Tile::Empty => {
+                if let Some(next_tile) = get_neighbor_for_direction(
+                    beam_end.0, beam_end.1, beam_end.2, total_rows, total_cols,
+                ) {
+                    let next_tile = (next_tile.0, next_tile.1, beam_end.2);
+                    if energized_tiles.insert(next_tile) {
+                        beam_ends.push(next_tile);
+                    }
+                }
+            }
+            Tile::LeftDiagonalMirror => {
+                let next_direction = match beam_end.2 {
+                    Direction::Up => Direction::Left,
+                    Direction::Right => Direction::Down,
+                    Direction::Left => Direction::Up,
+                    Direction::Down => Direction::Right,
+                };
+                let next_tile = get_neighbor_for_direction(
+                    beam_end.0,
+                    beam_end.1,
+                    next_direction,
+                    total_rows,
+                    total_cols,
+                );
+                if let Some(next_tile) = next_tile {
+                    let next_tile = (next_tile.0, next_tile.1, next_direction);
+                    if energized_tiles.insert(next_tile) {
+                        beam_ends.push(next_tile);
+                    }
+                }
+            }
+            Tile::RightDiagonalMirror => {
+                let next_direction = match beam_end.2 {
+                    Direction::Up => Direction::Right,
+                    Direction::Right => Direction::Up,
+                    Direction::Left => Direction::Down,
+                    Direction::Down => Direction::Left,
+                };
+                let next_tile = get_neighbor_for_direction(
+                    beam_end.0,
+                    beam_end.1,
+                    next_direction,
+                    total_rows,
+                    total_cols,
+                );
+                if let Some(next_tile) = next_tile {
+                    let next_tile = (next_tile.0, next_tile.1, next_direction);
+                    if energized_tiles.insert(next_tile) {
+                        beam_ends.push(next_tile);
+                    }
+                }
+            }
+            Tile::HorizontalSplitter => {
+                let next_directions: Vec<Direction> = match beam_end.2 {
+                    Direction::Up | Direction::Down => vec![Direction::Right, Direction::Left],
+                    dir => vec![dir],
+                };
+                for next_direction in next_directions {
+                    let next_tile = get_neighbor_for_direction(
+                        beam_end.0,
+                        beam_end.1,
+                        next_direction,
+                        total_rows,
+                        total_cols,
+                    );
+                    if let Some(next_tile) = next_tile {
+                        let next_tile = (next_tile.0, next_tile.1, next_direction);
+                        if energized_tiles.insert(next_tile) {
+                            beam_ends.push(next_tile);
+                        }
+                    }
+                }
+            }
+            Tile::VerticalSplitter => {
+                let next_directions: Vec<Direction> = match beam_end.2 {
+                    Direction::Left | Direction::Right => vec![Direction::Up, Direction::Down],
+                    dir => vec![dir],
+                };
+                for next_direction in next_directions {
+                    let next_tile = get_neighbor_for_direction(
+                        beam_end.0,
+                        beam_end.1,
+                        next_direction,
+                        total_rows,
+                        total_cols,
+                    );
+                    if let Some(next_tile) = next_tile {
+                        let next_tile = (next_tile.0, next_tile.1, next_direction);
+                        if energized_tiles.insert(next_tile) {
+                            beam_ends.push(next_tile);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let unique_energized_tiles = energized_tiles
+        .iter()
+        .map(|tile| (tile.0, tile.1))
+        .collect::<HashSet<(u32, u32)>>();
+    let count = unique_energized_tiles.len();
+    println!("{count}");
+}
+
+fn get_neighbor_for_direction(
+    row: u32,
+    col: u32,
+    direction: Direction,
+    total_rows: usize,
+    total_cols: usize,
+) -> Option<(u32, u32)> {
+    let offset = get_neighbor_offset_for_direction(direction);
+    let next_tile_row = row as i32 + offset.0;
+    let next_tile_col = col as i32 + offset.1;
+    if next_tile_row >= 0
+        && next_tile_row < total_rows as i32
+        && next_tile_col >= 0
+        && next_tile_col < total_cols as i32
+    {
+        return Some((next_tile_row as u32, next_tile_col as u32));
+    }
+    None
+}
+
+fn get_neighbor_offset_for_direction(direction: Direction) -> (i32, i32) {
+    match direction {
+        Direction::Up => (-1, 0),
+        Direction::Down => (1, 0),
+        Direction::Left => (0, -1),
+        Direction::Right => (0, 1),
+    }
+}
